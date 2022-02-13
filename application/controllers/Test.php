2121e6274ddc701b18777cf9e4377cf61ea4139f
EyeTracker <imamrasyid@gmail.com>
2022-02-13 14:28:12 +0700

    Some Changes

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -12,27 +12,11 @@ Class Test extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->helper('file');
-        $this->load->library('lib');
     }
 
     function index()
     {
-        $file = read_file('./darkblow_config.json');
-        $decode = json_decode($file);
-
-        foreach ($decode as $row)
-        {
-            echo $row->GoogleConfig->ClientID;
-        }
-    }
-
-    function encrypt()
-    {
-        $encrypt_string = $this->lib->Encrypt('Test Encrypt');
-        $decrypt_string = $this->lib->Decrypt($encrypt_string);
-
-        echo 'Encrypted String = ' . $encrypt_string . '<br> Decrypted String = ' . $decrypt_string . '.';
+        
     }
 }
 
