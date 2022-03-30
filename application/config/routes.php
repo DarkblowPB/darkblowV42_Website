c827e3d1ae3beb04b2ab1c8a090c1284df07fed5
EyeTracker <imamrasyid@gmail.com>
2022-03-30 11:04:11 +0700

    $&

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -51,7 +51,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'home';
 
-$route['adm'] = 'adm/login';
+$route['adm'] = 'adm/dashboard';
+
+$route['adm2'] = 'adm2/dashboard';
 
 $route['sitemap\.xml'] = 'sitemap';
 
