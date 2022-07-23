b4f28623fbd862f34ec16bb51be8f3a191ee9299
EyeTracker <imamrasyid@gmail.com>
2022-07-24 00:00:18 +0700

    Fixing Encryption Settings

--- application/libraries/Lib.php
+++ application/libraries/Lib.php
@@ -177,8 +177,10 @@ class Lib
 		);
 		$query = $this->ci->db->query('SELECT * FROM information_schema.tables')->result_array();
 		foreach ($query as $row) {
-			if ($this->ci->db->truncate($row['table_name'])) $data['success']++;
-			else $data['failed']++;
+			if ($row->table_type == "BASE_TABLE") {
+				if ($this->ci->db->truncate($row['table_name'])) $data['success']++;
+				else $data['failed']++;
+			}
 		}
 	}
 
